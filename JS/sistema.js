fcafff99dc6d0c364b69b7dc964d3d5a0e387915
LuizHenrique <luizhenrique9x08@gmail.com>
2022-12-03 21:21:16 -0300

    Novos Produtos

--- JS/sistema.js
+++ JS/sistema.js
@@ -16,11 +16,47 @@ const produtos = [
         nome: 'GIGABYTE 3080',
         descricao: 'Placa de Video',
         preco: 7899.99
+    },
+    {
+        img: 'images/gabinete-01.webp',
+        nome: 'Gabinete Mancer',
+        descricao: 'Gabinete Black Gamer',
+        preco: 450.99
+    },
+    {
+        img: 'images/gabinete-02.jpg',
+        nome: 'Gabinete Mancer',
+        descricao: 'Mancer Goblin',
+        preco: 639.99
+    },
+    {
+        img: 'images/microfone-01.jpg',
+        nome: 'Microfone HyperX',
+        descricao: 'Microfone RGB Preto',
+        preco: 750.00
+    },
+    {
+        img: 'images/placamae-02.jpg',
+        nome: 'Placa Mãe GIGABYTE',
+        descricao: 'Placa Mãe',
+        preco: 460.00
+    },
+    {
+        img: 'images/ryzen55600g-01.jpg',
+        nome: 'Processador Ryzen5',
+        descricao: 'Ryzen5 5600G',
+        preco: 939.99
+    },
+    {
+        img: 'images/ryzen75700g-01.jpg',
+        nome: 'Processador Ryzen7',
+        descricao: 'Ryzen7 5700G',
+        preco: 1550.00
     }
 ]
 
 
-duplicarItem = () => {
+produtosProGaming = () => {
     let saidaInput = document.querySelector('div.container_produtos')
         produtos.map((value)=>{
             saidaInput.innerHTML += `
@@ -43,4 +79,4 @@ duplicarItem = () => {
         })
 }
 
-duplicarItem()
+produtosProGaming()
